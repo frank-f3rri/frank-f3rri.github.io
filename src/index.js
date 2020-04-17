883db3d6747190b7769f992f5931ba68ee7329fe
arjun-bhatt <46908672+arjun-bhatt@users.noreply.github.com>
2020-04-16 22:16:37 -0400

    sa3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-const $ = require('jquery');
+/* eslint-disable indent */
 
-$('#main').html('Here we go!');
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+let time = 0;
+
+
+function updateClock() {
+    time += 1;
+    $('#main').html(`You have been on this page for ${String(time)} seconds.`);
+    console.log('update called!');
+}
+
+window.setInterval(updateClock, 1000);
