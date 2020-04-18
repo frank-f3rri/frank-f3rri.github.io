b28d968f1bc0fa8520221fd25fdb5ce9bc86ea95
arjun-bhatt <46908672+arjun-bhatt@users.noreply.github.com>
2020-04-18 18:42:57 -0400

    adding changes to starterpack

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@
 // change require to es6 import style
 import $ from 'jquery';
 import './style.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+
+const App = () => <div className="test">All the REACT are belong to us!</div>;
+
+ReactDOM.render(<App />, document.getElementById('main'));
 let time = 0;
 
 
