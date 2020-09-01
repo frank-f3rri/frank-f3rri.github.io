eb28fcc3b749f31ff5954e914914cb1d6c66fbaf
Weiling Huang <huanweiling@gmail.com>
2020-09-01 00:52:37 -0700

    #198 #199 #200 Landing page additional mockups, app store download, about the team

--- src/containers/nav.js
+++ src/containers/nav.js
@@ -5,14 +5,12 @@ import '../style.scss';
 class Nav extends Component {
   render() {
     return (
-      <div className="navContainer">
+      <div className="container">
         <nav>
           <ul className="navBar">
             <li><NavLink className="navItem" exact to="/"> Home </NavLink></li>
-            <li><NavLink className="navItem" to="/whatsup"> Explore </NavLink></li>
-            <li><NavLink className="navItem" to="/create"> Create Portal </NavLink></li>
-            <li><NavLink className="navItem" to="/legal"> Legal </NavLink></li>
-            <li><NavLink className="donateItem" to="https://www.buymeacoffee.com/whosdown"><span> Donate </span> </NavLink></li>
+            <li><NavLink className="navItem" to="/about"> About </NavLink></li>
+            <li><a className="donateItem" href="https://www.buymeacoffee.com/whosdown"><span> Donate! </span> </a></li>
           </ul>
         </nav>
       </div>
